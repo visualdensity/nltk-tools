39308ede14e96ddfca09e5db49824fc6bdff42dd
Wee Keat Chin <weekeat@visualdensity.com>
2013-06-11 10:27:40 +1000

    Renamed folder

--- indexer.py
+++ indexer.py
@@ -9,14 +9,16 @@ is created for use with NLTK's corpus library.
 """
 import os
 
-f = open("./cats.txt", 'w')
+index_file    = 'cats.txt'
+training_path = 'trainig/'
 
-for top, dirs, files in os.walk('content/'):
+f = open(index_file, 'w')
+
+for top, dirs, files in os.walk(training_path):
     for nm in files:
         filepath = os.path.join(top, nm)
         filepath_chunk = filepath.split("/")
         cat_line = filepath + " " + filepath_chunk[-2] + "\n"
 
         f.write(cat_line)
-
 f.closed 
