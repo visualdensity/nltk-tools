11ac86c9183ca24122af3c8179af381ff24192b0
Wee Keat Chin <weekeat@visualdensity.com>
2013-06-11 11:22:24 +1000

    added check

--- indexer.py
+++ indexer.py
@@ -12,6 +12,14 @@ import os
 index_file    = 'cats.txt'
 training_path = 'training/'
 
+if not os.path.exists(training_path):
+    print "Input path " + training_path + " not found."
+    sys.exit()
+
+if not os.access( index_file, os.W_OK):
+    print "Target file " + training_path + " could not be created. Permission issue?"
+    sys.exit()
+
 f = open(index_file, 'w')
 
 for top, dirs, files in os.walk(training_path):
